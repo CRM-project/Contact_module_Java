ef408af5f94567f5139b5c57d67154a68230cefb
milomory <milomory@gmail.com>
2015-12-03 22:53:31 +0300

    fix sintax misteke=)

--- src/main/java/net/bonvio/dao/entityImpl/AnswerDaoImpl.java
+++ src/main/java/net/bonvio/dao/entityImpl/AnswerDaoImpl.java
@@ -1,19 +1,15 @@
 package net.bonvio.dao.entityImpl;
 
 import net.bonvio.dao.GenericDAOImpl;
-import net.bonvio.dao.entity.AnswerDao;
 import net.bonvio.model.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
 
 /**
  * Created by mil on 02.12.15.
  */
 
 @Repository
-public class AnswerDaoImpl extends GenericDAOImpl<Answer> implements AnswerDao{
+public class AnswerDaoImpl extends GenericDAOImpl<Answer> {
 
     public AnswerDaoImpl() {
         System.out.println("Created AnswerDaoImpl");
